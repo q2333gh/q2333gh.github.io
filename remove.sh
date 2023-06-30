382bfe0412c88d52b8ec858830dff31bc2524861
btwl <1253102853@qq.com>
2023-06-30 17:45:00 +0800

    update

---
+++ remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git rm .editorconfig
+git rm .gitattributes
+git rm.github
+git rm /docs
+git rm /test
+git rm CHANGELOG.md
+git rm minimal-mistakes-jekyll.gemspec
+git rm README.md
+git rm screenshot-layouts.png
+git rm screenshot.png
