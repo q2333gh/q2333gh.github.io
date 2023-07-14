fa29178650842de2e3eaa9beabcae230833be7e5
btwl <1253102853@qq.com>
2023-07-14 18:41:26 +0800

    try upload and display a lot imgs , test the net performance

--- handy_scripts/docx_to_md/convertDocx2md.sh
+++ handy_scripts/docx_to_md/convertDocx2md.sh
@@ -22,6 +22,7 @@ pandoc --extract-media ../assets/imgs/java java1.docx -o ../../_posts/Java.md
 
 # bigger problem: if i have 300 picture in  a blog ,
 # github pages load speed? should i use another OSS to storage the imgs?
+# deeper solution: compress the imgs to a quality about show enough clearity.
 
 
 
